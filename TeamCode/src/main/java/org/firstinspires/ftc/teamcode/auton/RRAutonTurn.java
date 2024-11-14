c33b62ab339c29af75be717a150639b316a06057
sivrim <64481915+sivrim@users.noreply.github.com>
2024-11-13 19:56:35 -0500

    disable unused teleops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RRAutonTurn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RRAutonTurn.java
@@ -4,11 +4,13 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.auton.drive.SampleMecanumDrive;
 
 @Config
+@Disabled
 @Autonomous(group = "ff")
 public class RRAutonTurn extends LinearOpMode {
     public static final double FORWARD_1 = 20;
