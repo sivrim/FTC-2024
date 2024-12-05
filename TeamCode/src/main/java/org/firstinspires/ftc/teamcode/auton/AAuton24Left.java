b942a92f9356a057895b6ce6568c1cc710e8d60e
sivrim <64481915+sivrim@users.noreply.github.com>
2024-12-04 19:45:32 -0500

    Before Anirvin touches keyboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AAuton24Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AAuton24Left.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.auton;
 
-import android.database.sqlite.SQLiteException;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -35,10 +33,12 @@ public class AAuton24Left extends ArmUp {
     Servo clawServo;
     Servo wristServo;
     DcMotor armMotor2;
-
+    SampleMecanumDrive drive;
+    Trajectory sample1_trajectoryForwardFromStart, sample1_trajectoryStrafeLeftToBasket, sample1_trajectoryGoBack;
+    Trajectory trajectoryForwardToStart, trajectoryForwardToTurn;
     @Override
     public void runOpMode() {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive = new SampleMecanumDrive(hardwareMap);
         armMotor = hardwareMap.get(DcMotor.class, DeviceNames.MOTOR_ARM);
         armMotor2 = hardwareMap.get(DcMotor.class, DeviceNames.MOTOR_ARM2);
         clawServo = hardwareMap.get(Servo.class, DeviceNames.SERVO_CLAW);
@@ -46,48 +46,19 @@ public class AAuton24Left extends ArmUp {
         wristServo = hardwareMap.get(Servo.class, DeviceNames.SERVO_WRIST);
         wristServo.setPosition(MAX_WRIST_UP);
 
-        Trajectory trajectoryForwardFromStart = drive.trajectoryBuilder(new Pose2d())
-                .forward(FORWARD_FROM_START_STEP_1)
-                .build();
+        /**
+         * arm 1 --> goes down on forward
+         * arm 2 --> goes up on forward
+         */
 
-        Trajectory trajectoryStrafeLeftToBasket = drive.trajectoryBuilder(trajectoryForwardFromStart.end())
-                .strafeLeft(STRAFE_LEFT_GO_TO_BASKET_STEP_2)
-                .build();
-
-        Trajectory trajectoryGoBack = drive.trajectoryBuilder(trajectoryStrafeLeftToBasket.end())
-                .back(BACK_STEP_3)
-                .build();
-
-        Trajectory trajectoryForwardToStart = drive.trajectoryBuilder(trajectoryGoBack.end())
-                .forward(FORWARD_TO_START_STEP_4)
-                .build();
-
-        Trajectory trajectoryForwardToTurn = drive.trajectoryBuilder(trajectoryForwardToStart.end())
-                .forward(STRAFE_TO_PARK_STEP_5)
-                .build();
+        //do this on init, before start, as each trajectory building can take up to .5 seconds.
+        initTrajectories();
 
         waitForStart();
 
         if(isStopRequested()) return;
 
-        moveArmToPosition(DcMotorSimple.Direction.FORWARD, (int)(10 * ARM1_ANGLE_TO_ENCODER), armMotor, runtime);
-
-        drive.followTrajectory(trajectoryForwardFromStart);
-
-        /**
-         * arm 1 --> goes down by default
-         * arm 2 --> goes back by default
-         */
-
-        moveArmToPosition(DcMotorSimple.Direction.REVERSE, (int)(ARM_2_MOVE_BACK_1_ANGLE * ARM2_ANGLE_TO_ENCODER), armMotor2, runtime);
-        moveArmToPosition(DcMotorSimple.Direction.FORWARD, (int)(ARM_1_MOVE_BACK_1_ANGLE * ARM1_ANGLE_TO_ENCODER), armMotor, runtime);
-        moveArmToPosition(DcMotorSimple.Direction.REVERSE, (int)(ARM_2_MOVE_BACK_2_ANGLE * ARM2_ANGLE_TO_ENCODER), armMotor2, runtime);
-        wristServo.setPosition(MAX_WRIST_DROP);
-        drive.followTrajectory(trajectoryStrafeLeftToBasket);
-        drive.turn(Math.toRadians(TURN_M45 * TURN_RATIO));
-        drive.followTrajectory(trajectoryGoBack);
-        clawServo.setPosition(MAX_CLAW_OPEN);
-        wristServo.setPosition(MAX_WRIST_DOWN);
+        dropSample1();
 
         sleep(2000);
         drive.followTrajectory(trajectoryForwardToStart);
@@ -101,6 +72,47 @@ public class AAuton24Left extends ArmUp {
 
 
 
+    }
+
+    private void dropSample1() {
+        //move arm up so it does not drag against surface when moving
+        moveArmToPosition(DcMotorSimple.Direction.FORWARD, (int)(10 * ARM1_ANGLE_TO_ENCODER), armMotor, runtime);
+
+        drive.followTrajectory(sample1_trajectoryForwardFromStart);
+
+        //move arm up, while we are away from all walls and basket. Otherwise, moving arms has to be co-ordinated
+        moveArmToPosition(DcMotorSimple.Direction.REVERSE, (int)(ARM_2_MOVE_BACK_1_ANGLE * ARM2_ANGLE_TO_ENCODER), armMotor2, runtime);
+        moveArmToPosition(DcMotorSimple.Direction.FORWARD, (int)(ARM_1_MOVE_BACK_1_ANGLE * ARM1_ANGLE_TO_ENCODER), armMotor, runtime);
+        moveArmToPosition(DcMotorSimple.Direction.REVERSE, (int)(ARM_2_MOVE_BACK_2_ANGLE * ARM2_ANGLE_TO_ENCODER), armMotor2, runtime);
+
+        wristServo.setPosition(MAX_WRIST_DROP);
+        drive.followTrajectory(sample1_trajectoryStrafeLeftToBasket);
+        drive.turn(Math.toRadians(TURN_M45 * TURN_RATIO));
+        drive.followTrajectory(sample1_trajectoryGoBack);
+        clawServo.setPosition(MAX_CLAW_OPEN);
+        wristServo.setPosition(MAX_WRIST_DOWN);
+    }
+
+    private void initTrajectories() {
+        sample1_trajectoryForwardFromStart = drive.trajectoryBuilder(new Pose2d())
+                .forward(FORWARD_FROM_START_STEP_1)
+                .build();
+
+        sample1_trajectoryStrafeLeftToBasket = drive.trajectoryBuilder(sample1_trajectoryForwardFromStart.end())
+                .strafeLeft(STRAFE_LEFT_GO_TO_BASKET_STEP_2)
+                .build();
+
+        sample1_trajectoryGoBack = drive.trajectoryBuilder(sample1_trajectoryStrafeLeftToBasket.end())
+                .back(BACK_STEP_3)
+                .build();
+
+        trajectoryForwardToStart = drive.trajectoryBuilder(sample1_trajectoryGoBack.end())
+                .forward(FORWARD_TO_START_STEP_4)
+                .build();
+
+        trajectoryForwardToTurn = drive.trajectoryBuilder(trajectoryForwardToStart.end())
+                .forward(STRAFE_TO_PARK_STEP_5)
+                .build();
     }
 
 }
