ddd33aa4abb1c25c781ce52e3c9f45afcb826744
sivrim <64481915+sivrim@users.noreply.github.com>
2024-12-07 16:15:44 -0500

    trying trajectory sequence

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AAuton24Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AAuton24Left.java
@@ -9,11 +9,23 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.DeviceNames;
 import org.firstinspires.ftc.teamcode.auton.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.config.ArmDown;
 import org.firstinspires.ftc.teamcode.config.ArmUp;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
 import static org.firstinspires.ftc.teamcode.config.TurnConstants.*;
+
+import java.util.List;
+
 @Config
 @Autonomous(group = "aaa")
 public class AAuton24Left extends ArmUp {
@@ -34,8 +46,18 @@ public class AAuton24Left extends ArmUp {
     Servo wristServo;
     DcMotor armMotor2;
     SampleMecanumDrive drive;
+
+    Trajectory lastTrajectory;
     Trajectory sample1_trajectoryForwardFromStart, sample1_trajectoryStrafeLeftToBasket, sample1_trajectoryGoBack;
     Trajectory trajectoryForwardToStart, trajectoryForwardToTurn;
+    private Position cameraPosition = new Position(DistanceUnit.INCH,
+            0, 0, 0, 0);
+    private YawPitchRollAngles cameraOrientation = new YawPitchRollAngles(AngleUnit.DEGREES,
+            0, -90, 0, 0);
+
+    private AprilTagProcessor aprilTag;
+
+    private VisionPortal visionPortal;
     @Override
     public void runOpMode() {
         drive = new SampleMecanumDrive(hardwareMap);
@@ -53,10 +75,12 @@ public class AAuton24Left extends ArmUp {
 
         //do this on init, before start, as each trajectory building can take up to .5 seconds.
         initTrajectories();
-
+        initAprilTag();
         waitForStart();
 
         if(isStopRequested()) return;
+
+        //List<AprilTagDetection> currentDetections = aprilTag.getDetections();
 
         dropSample1();
 
@@ -70,9 +94,65 @@ public class AAuton24Left extends ArmUp {
         clawServo.setPosition(MAX_CLAW_CLOSE);
         clawServo.setPosition(MAX_WRIST_DROP);
 
+    }
 
+    public void moveReadCorrect(Pose2d start, Pose2d end) {
+        move(start, end);
+        Pose2d currentPose = readAprilTag();
+        if(currentPose != null) {
+            //TODO add leniency
+            boolean withinMarginOfError = Math.abs(currentPose.getX() - end.getX()) < .2
+                    && Math.abs(currentPose.getY() - end.getY()) < .2
+                    && Math.abs(currentPose.getHeading() - end.getHeading()) < Math.toRadians(2);
+
+            if(!withinMarginOfError) {
+                move(currentPose, end);
+            }
+        }
+    }
+
+    public void move(Pose2d start, Pose2d end) {
+        Pose2d startOfTrajectory = lastTrajectory == null ? new Pose2d() : lastTrajectory.end();
+
+        lastTrajectory = drive.trajectoryBuilder(startOfTrajectory)
+                .lineToLinearHeading(new Pose2d(end.getX() - start.getX(), end.getY() - start.getY(), end.getHeading() - start.getHeading()))
+                .build();
+
+        drive.followTrajectory(lastTrajectory);
 
     }
+
+    public Pose2d readAprilTag() {
+        visionPortal.resumeStreaming();
+        List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+        visionPortal.stopStreaming();
+        for (AprilTagDetection detection : currentDetections) {
+
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                Position position = detection.robotPose.getPosition();
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)",
+                        position.x,
+                        position.y,
+                        position.z));
+                YawPitchRollAngles orientation = detection.robotPose.getOrientation();
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)",
+                        orientation.getPitch(AngleUnit.DEGREES),
+                        orientation.getRoll(AngleUnit.DEGREES),
+                        orientation.getYaw(AngleUnit.DEGREES)));
+                if(detection.id == 13 || detection.id == 16){
+                    return new Pose2d(position.x, position.y, orientation.getYaw());
+                }
+
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
+        return null;
+    }
+
 
     private void dropSample1() {
         //move arm up so it does not drag against surface when moving
@@ -112,6 +192,22 @@ public class AAuton24Left extends ArmUp {
 
         trajectoryForwardToTurn = drive.trajectoryBuilder(trajectoryForwardToStart.end())
                 .forward(STRAFE_TO_PARK_STEP_5)
+                .build();
+
+
+    }
+
+    private void initAprilTag() {
+
+        aprilTag = new AprilTagProcessor
+                .Builder()
+                .setCameraPose(cameraPosition, cameraOrientation)
+                .build();
+
+        visionPortal = new VisionPortal
+                .Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(aprilTag)
                 .build();
     }
 
